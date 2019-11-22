3ea3d32b9f5d5d7a95c50eadc227b2235ffd9cef
nmvsaikumarb3 <42762591+nmvsaikumarb3@users.noreply.github.com>
2019-11-22 12:04:55 +0530

    Add files via upload

---
+++ tank.ino
@@ -0,0 +1,75 @@
+ #include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(9, 10);
+// defines pins numbers                //1
+const int trigPin = 9;
+const int echoPin = 10;
+// defines variables
+long duration;
+int distance;                          //1
+
+void setup()
+{
+  mySerial.begin(9600);   // Setting the baud rate of GSM Module  
+  Serial.begin(9600);    // Setting the baud rate of Serial Monitor (Arduino)
+  delay(100);
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output         //2
+pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+Serial.begin(9600); // Starts the serial communication              //2
+}
+
+
+void loop()
+{
+ // Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+distance= duration*0.034/2;
+// Prints the distance on the Serial Monitor
+Serial.print("Distance: ");
+Serial.println(distance);
+}
+
+
+  if (Serial.available()>0)
+   switch(Serial.read())
+  {
+    case 's':
+      SendMessage();
+      break;
+    case 'r':
+      RecieveMessage();
+      break;
+  }
+{
+ if (mySerial.available()>0)
+   Serial.write(mySerial.read());
+}
+
+
+ void SendMessage()
+{
+  mySerial.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
+  delay(1000);  // Delay of 1000 milli seconds or 1 second
+  mySerial.println("AT+CMGS=\"+919491548500\"\r"); // Replace x with mobile number
+  delay(1000);
+  mySerial.println("I am SMS from GSM Module");// The SMS text you want to send
+  delay(100);
+   mySerial.println((char)26);// ASCII code of CTRL+Z
+  delay(1000);
+}
+
+
+ void RecieveMessage()
+{
+  mySerial.println("AT+CNMI=2,2,0,0,0"); // AT Command to receive a live SMS
+  delay(1000);
+ }
+ 
